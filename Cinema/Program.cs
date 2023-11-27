352a9c96bf76384f091b6dc5ce37e7b2289032b0
Vinícius Bernardes <vinicius@bernardes.dev>
2023-11-26 21:42:47 -0300

    feat: add IRepository injection and fix pg connection

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -1,3 +1,4 @@
+using Cinema.Data;
 using Cinema.Helpers;
 using Cinema.Services.Sessions;
 
@@ -6,12 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddDbContext<DataContext>();
     builder.Services.AddControllers();
     builder.Services.AddScoped<ISessionService, SessionService>();
+    builder.Services.AddScoped<IRepository, Repository>();
 }
 
 var app = builder.Build();
 {
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
+    await app.RunAsync();
 }
